ccfb5d549a3394bffa050ca1b0b831c1b3d0eeb7
hye-yoen <tgeelhy@gmail.com>
2025-10-29 12:40:24 +0900

    save 10/29

--- 05/src/App.js
+++ 05/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Component_01 from './05조건부랜더링/01Component';
 import Component_02 from './05조건부랜더링/02Component';
+import Component_03 from './05조건부랜더링/03Component';
 
 function App() {
   //State 컴포넌트 내에서 유지 시켜야 할 값
@@ -14,9 +15,12 @@ function App() {
       <h1>조건부 랜더링</h1>
       {/* {if} */}
       <Component_01 isAuth={true} />
+      <hr/>
       {/* {삼항연산자} */}
-      <Component_02 isAuth={true}/>
+      <Component_02 isAuth={true} />
+      <hr/>
       {/* {&&} */}
+      <Component_03 items={['aaa','bbb','ccc','ddd']}/>
     </div>
   );
 }
